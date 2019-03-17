cde0ef798abb25a0bbd44110ba454da4b6d9776a
nb20102010 <lirunliruntc@gmail.com>
2019-03-16 23:14:56 -0600

    Finished Lecture 5

--- src/com/hsp/inter/App1.java
+++ src/com/hsp/inter/App1.java
@@ -6,9 +6,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App1 {
 	
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("com/hsp/inter/Beans.xml");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/hsp/inter/beans.xml");
 		changeLetterCase clc = (changeLetterCase)ac.getBean("changeLetter");
 		System.out.println(clc.change());
+		
+		Student stu1 = (Student)ac.getBean("student1");
+		Student stu2 = (Student)ac.getBean("student1");
+		stu1.print();
+		stu2.print();
+		
+		stu1 = (Student)ac.getBean("student2");
+		stu2 = (Student)ac.getBean("student2");
+		stu1.print();
+		stu2.print();
 	}
 	
 }
